c9a42d469718205d9f64532b2abdc9d93725727a
itsBehr <162396437+itsBehr@users.noreply.github.com>
2025-04-14 20:29:13 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-curl -L [update here] 
+curl -L https://github.com/itsBehr/FedoraFastSpinupTest/raw/refs/heads/maincandidate-v2/update >> ~/.local/FFS.sh
+curl -L "https://github.com/itsBehr/FedoraFastSpinupTest/raw/refs/heads/maincandidate-v2/pkg-install-preamble" >> ~/.local/FFS3.sh
+echo "#!/bin/bash" >> ~/.local/FFS2.sh
 
-rm -f ~/Desktop/init.sh
 
 echo "Let's get started! What type of CPU do you use?"
 read -p '(a)md or (i)ntel | ' cpuType
@@ -16,29 +17,30 @@ read -p '(y)es or (n)o | ' vrOpt
 
 if [ $cpuType = a ]
 then
-curl -L [AMDCPUFILE] > ~/Desktop/fedoraquikstart.sh
+curl -L [AMDCPUFILE] >> ~/Desktop/fedoraquikstart.sh
 elif [ $cpuType = i ]
 then
-curl -L [INTELCPU] > ~/Desktop/fedoraquikstart.sh
+curl -L [INTELCPU] >> ~/Desktop/fedoraquikstart.sh
 else
 echo "Please insert valid arguments." && exec "$ScriptLoc"
 fi
 
 
 if [ $gpuType = y ]
-curl -L [NVIDIAGPU] > ~/Desktop/fedoraquikstart.sh
+curl -L [NVIDIAGPU] >> ~/Desktop/fedoraquikstart.sh
 elif [ $gpuType = n ]
 then
-curl -L [nostep2] > ~/Desktop/fedoraquikstart.sh
+curl -L [nostep2] >> ~/Desktop/fedoraquikstart.sh
 else 
 echo "Please insert valid arguments." && exec "$ScriptLoc"
 fi
 
 if [ $vrOpt = n ]
-curl -L [NOVR] > ~/Desktop/fedoraquikstart.sh
+curl -L [NOVR] >> ~/Desktop/fedoraquikstart.sh
 elif [ $vrOpt = y ]
 then
-curl -L https://github.com/itsBehr/FedoraFastSpinupTest/raw/refs/heads/itsBehr-maincandidate/Step_1I-NV > ~/Desktop/fedoraquikstart.sh
+curl -L https://github.com/itsBehr/FedoraFastSpinupTest/raw/refs/heads/itsBehr-maincandidate/Step_1I-NV >> ~/Desktop/fedoraquikstart.sh
 else
 echo "Please insert valid arguments." && exec "$ScriptLoc"
 fi
+
